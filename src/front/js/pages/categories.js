30cf7589d8dbd19f9cc6ba2bb70c01867b6e049a
igreen20 <iyahnagreen1@gmail.com>
2023-03-01 23:35:28 +0000

    changes to Categories page

--- src/front/js/pages/categories.js
+++ src/front/js/pages/categories.js
@@ -1,63 +1,58 @@
+import React from "react";
+import "../../styles/roomSelector.css";
 
-import React, { useState } from "react";
-export const CarouselItem = ({ children, width }) => {
+export default function roomImages() {
   return (
-    <div className="carousel-item" style={{ width: width }}>
-      {children}
-    </div>
-  );
-};
-
-const Carousel = ({ children }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const updateIndex = (newIndex) => {
-    if (newIndex < 0) {
-      newIndex = 0;
-    } else if (newIndex >= React.Children.count(children)) {
-      newIndex = React.Children.count(children) - 1;
-    }
-    setActiveIndex(newIndex);
-  };
-  return (
-    <div className="carousel">
-      <div
-        className="inner"
-        style={{ transform: "translateX(-${activeIndex * 100}%)" }}
-      >
-        {React.Children.map(children, (child, index) => {
-          return React.cloneElement(child, { width: "100%" });
-        })}
+    <div className="container">
+      <div className="room1">
+        <img
+          src="https://i.imgur.com/SmhGAwI.png"
+          alt="Dude, Where's my Closet?"
+        />
+        <h4>Dude, Where's my Closet?</h4>
       </div>
-      <div className="indicators">
-        <button
-          onClick={() => {
-            updateIndex(activeIndex - 1);
-          }}
-        >
-          Prev
-        </button>
-        {React.Children.map(children, (child, index) => {
-          return (
-            <button
-              onClick={() => {
-                updateIndex(index);
-              }}
-            >
-              {index + 1}
-            </button>
-          );
-        })}
-        <button
-          onClick={() => {
-            updateIndex(activeIndex + 1);
-          }}
-        >
-          Next
-        </button>
+      <div className="room2">
+        <img src="https://i.imgur.com/MT1HV5j.png" alt="geeks4sleep" />
+        <h4>geeks4sleep</h4>
+      </div>
+      <div className="room3">
+        <img src="https://i.imgur.com/9WdnFoR.png" alt="Lazy Susan's Kitchen" />
+        <h4>Lazy Susan's Kitchen</h4>
+      </div>
+      <div className="room4">
+        <img
+          src="https://i.imgur.com/tPG1Meg.png"
+          alt="Quick Bites, Long Talks"
+        />
+        <h4>Quick Bites, Long Talks</h4>
+      </div>
+      <div className="room5">
+        <img
+          src="https://i.imgur.com/jG5GOPd.png"
+          alt="The Fancy Man's Study"
+        />
+        <h4>The Fancy Man's Study</h4>
+      </div>
+      <div className="room6">
+        <img src="https://i.imgur.com/YpAijUT.png" alt="Straight As to Zzz" />
+        <h4>Straight As to Zzz</h4>
+      </div>
+      <div className="room7">
+        <img src="https://i.imgur.com/HHILH8C.png" alt="Tweenage Dirtbag" />
+        <h4>Tweenage Dirtbag</h4>
+      </div>
+      <div className="room8">
+        <img src="https://i.imgur.com/iaJefXz.png" alt="Unicorn Dreams" />
+        <h4>Unicorn Dreams</h4>
+      </div>
+      <div className="room9">
+        <img src="https://i.imgur.com/2W36O5V.png" alt="Vroom Room" />
+        <h4>Vroom Room</h4>
+      </div>
+      <div className="room10">
+        <img src="https://i.imgur.com/6aHMhet.png" alt="Zen Den" />
+        <h4>Zen Den</h4>
       </div>
     </div>
   );
-};
-
-export default Carousel;
+}
