710a4e7f3ee948ac9d0916c376c34f37fa6c86f2
BrodyDubach <brodydubach@gmail.com>
2023-03-17 00:07:19 +0000

    working on login/signup

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,35 +1,7 @@
 import React, { useState } from 'react';
 import '/workspace/sims-rooms-irl/src/front/styles/signup.css';
 
-// const SignupForm = () => {
-//     const [username, setUsername] = useState('');
-//     const [password, setPassword] = useState('');
 
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-
-//         // Submit username/password here
-//     }
-
-//     return (
-//         <form className="signupForm" onSubmit={handleSubmit}>
-//             <input
-//                 type="text"
-//                 placeholder="Username"
-//                 value={username}
-//                 onChange={(e) => setUsername(e.target.value)}
-//               />
-//               <input
-//                 type="password"
-//                 placeholder="Password"
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-//               />
-//               <button type="submit">Login</button>
-//               <button type="submit">Register</button>
-//        </form>
-//     );
-// };
 
 class SignupForm extends React.Component {
     constructor(props) {
